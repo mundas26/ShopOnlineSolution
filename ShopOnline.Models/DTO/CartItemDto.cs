c2e24212c34386ee54329482713469e3ec9c009f
Juniel Mundas <Juniel Mundas@MUNDAS26>
2023-11-19 12:24:02 +0800

    Add project files.

---
+++ ShopOnline.Models/DTO/CartItemDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models.DTO
+{
+    public class CartItemDto
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public string ProductImageURL { get; set; } = string.Empty;
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
